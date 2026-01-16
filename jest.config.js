fba87184f333f9e8e7ee6a67d2ab0859cbb6135b
karan sengar <raazsengar1212@gmail.com>
2026-01-16 20:10:53 +0530

    chore: update sonar config

--- jest.config.js
+++ jest.config.js
@@ -4,19 +4,34 @@ module.exports = {
   testEnvironment: "node",
   collectCoverage: true,
   coverageProvider: "v8",
+
   collectCoverageFrom: ["src/**/*.ts", "!tests/**", "!**/node_modules/**"],
+
   coveragePathIgnorePatterns: [
     "/node_modules/",
     "/src/migrations/",
     "/src/types/",
     "/src/index.ts",
   ],
+
   roots: ["<rootDir>/src"],
   moduleFileExtensions: ["ts", "js"],
   clearMocks: true,
+
   transform: {
     "^.+\\.ts$": "ts-jest",
     "^.+\\.js$": "babel-jest",
   },
+
   transformIgnorePatterns: ["/node_modules/(?!(mock-jwks|until-async|msw)/)"],
+
+  // 🔥 THIS IS THE IMPORTANT PART
+  coverageThreshold: {
+    global: {
+      branches: 40,
+      functions: 40,
+      lines: 40,
+      statements: 40,
+    },
+  },
 };
